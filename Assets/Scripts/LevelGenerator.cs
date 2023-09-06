46bca365ea3fb5c633e769417b6767652e3b8746
beetleberries <93026704+beetleberries@users.noreply.github.com>
2023-09-06 22:45:31 +1000

    should be level gen

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,19 +26,55 @@ public class LevelGenerator : MonoBehaviour
  {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
  }; 
     public Tile[] tiles;
-    public Vector3Int position;
+
     public Tilemap tilemap;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hi");
-        //int i = levelMap[0,0];
-        tilemap.SetTile(position,tiles[0]);
-        tilemap.SetTile(position + Vector3Int.left,tiles[1]);
+
+        BuildLevel();
         
-        
+    }
+
+    void BuildLevel()
+    {
+        for (int x = 0; x < levelMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < levelMap.GetLength(1); y++)
+            {
+                int type = getType(x,y);
+
+                tilemap.SetTile(new Vector3Int(y - 5, 4 - x,0), tiles[type]);
+
+                Matrix4x4 m = Matrix4x4.Rotate(getRotation(type, x, y));
+                tilemap.SetTransformMatrix(new Vector3Int(y - 5, 4 - x,0),m);
+            }
+        }
+    }
+
+    private int getType(int x, int y)
+    {
+        x = Math.Min(levelMap.GetLength(0), x);
+        y = Math.Min(levelMap.GetLength(1), y);
+        return levelMap[x, y];
+    }
+
+    private Quaternion getRotation(int type, int x, int y)
+    {
+        if (type == 2)
+        {
+            int below = getType(x+1,y);
+            if (below == 5 || below == 0) return Quaternion.Euler(0, 0, 90);
+        }
+        if (type == 4)
+        {
+            int below = getType(x+1,y);
+            if (below == 5 || below == 0) return Quaternion.Euler(0, 0, 90);
+        }
+
+        return Quaternion.identity;
     }
 
     // Update is called once per frame
